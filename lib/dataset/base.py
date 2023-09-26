c0948d0cf35f209ebeee095d94b2b89516dd3d62
Chienping Tsung <jianpingzong@outlook.com>
2023-09-26 22:50:17 +0800

    dataset/lasot

---
+++ lib/dataset/base.py
@@ -0,0 +1,21 @@
+import abc
+from pathlib import Path
+
+
+class VideoDataset(abc.ABC):
+    def __init__(self, name, root):
+        self.name = name
+        self.root = Path(root)
+
+        self.video_list = []
+
+    def __len__(self):
+        return len(self.video_list)
+
+    @abc.abstractmethod
+    def get_frames(self, video_id, frame_ids):
+        pass
+
+    @abc.abstractmethod
+    def get_annotation(self, video_id):
+        pass
---
+++ lib/dataset/lasot.py
@@ -0,0 +1,56 @@
+import os
+
+import pandas
+from PIL import Image
+from easydict import EasyDict
+
+from lib.dataset.base import VideoDataset
+
+
+class LaSOT(VideoDataset):
+    def __init__(self, root, partition):
+        super().__init__('LaSOT', root)
+
+        self.video_list = self.build_video_list(partition)
+
+    def read_partition(self, path):
+        return pandas.read_csv(path, header=None).squeeze().to_numpy().tolist()
+
+    def read_full_occlusion(self, path):
+        return pandas.read_csv(path, header=None).squeeze().to_numpy()
+
+    def read_groundtruth(self, path):
+        return pandas.read_csv(path, header=None).to_numpy()
+
+    def read_nlp(self, path):
+        return pandas.read_csv(path, header=None).squeeze()
+
+    def read_out_of_view(self, path):
+        return pandas.read_csv(path, header=None).squeeze().to_numpy()
+
+    def build_video_list(self, partition):
+        class_list = [c for c in os.listdir(self.root)]
+        partition = self.read_partition(partition) if partition else None
+
+        video_list = []
+        for c in class_list:
+            for v in os.listdir(self.root.joinpath(c)):
+                if partition is None or v in partition:
+                    video_list.append(self.root.joinpath(c, v))
+
+        return video_list
+
+    def get_frames(self, video_id, frame_ids):
+        video_path = self.video_list[video_id].joinpath('img')
+
+        return [Image.open(video_path.joinpath(f'{id + 1:08}.jpg')) for id in frame_ids]
+
+    def get_annotation(self, video_id):
+        video_path = self.video_list[video_id]
+
+        occlusion = self.read_full_occlusion(video_path.joinpath('full_occlusion.txt'))
+        groundtruth = self.read_groundtruth(video_path.joinpath('groundtruth.txt'))
+        nlp = self.read_nlp(video_path.joinpath('nlp.txt'))
+        out_of_view = self.read_out_of_view(video_path.joinpath('out_of_view.txt'))
+
+        return EasyDict({'occlusion': occlusion, 'groundtruth': groundtruth, 'nlp': nlp, 'out_of_view': out_of_view})
