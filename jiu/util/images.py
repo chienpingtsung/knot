9d7624c0647ea099312618929a9dc14473ffd1a5
Chienping Tsung <jianpingzong@outlook.com>
2024-04-09 21:23:49 +0800

    Update images.py

--- jiu/util/images.py
+++ jiu/util/images.py
@@ -2,14 +2,15 @@ import torch
 import torchvision
 
 
-def draw_bounding_boxes(image, boxes, width, in_fmt):
+def draw_bounding_boxes(image, boxes, in_fmt, colors=None, width=1):
     """
 
     Args:
         image: Tensor image ranges in (0, 1) with shape (C, H, W).
         boxes: Boxes with shape (N, 4).
-        width:
         in_fmt: Boxes format.
+        colors:
+        width:
 
     Returns:
         image: Tensor image ranges in (0, 255) with shape (C, H, W).
@@ -17,20 +18,21 @@ def draw_bounding_boxes(image, boxes, width, in_fmt):
     image = (image * 255).to(torch.uint8)
     if in_fmt != 'xyxy':
         boxes = torchvision.ops.box_convert(boxes, in_fmt, 'xyxy')
-    return torchvision.utils.draw_bounding_boxes(image, boxes, width=width)
+    return torchvision.utils.draw_bounding_boxes(image, boxes, colors=colors, width=width)
 
 
-def batch_draw_bounding_boxes(images, boxes, width, in_fmt):
+def batch_draw_bounding_boxes(images, boxes, in_fmt, colors=None, width=1):
     """
 
     Args:
         images: Tensor images ranges in (0, 1) with shape (B, C, H, W).
         boxes: Boxes with shape (B, N, 4).
-        width:
         in_fmt: Boxes format.
+        colors:
+        width:
 
     Returns:
         images: Tensor images ranges in (0, 255) with shape (B, C, H, W).
     """
-    images = list(draw_bounding_boxes(image, box, width, in_fmt) for image, box in zip(images, boxes))
+    images = list(draw_bounding_boxes(image, box, in_fmt, colors, width) for image, box in zip(images, boxes))
     return torch.stack(images)
