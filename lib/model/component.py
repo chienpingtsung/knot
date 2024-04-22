66d7532bb06190dfa6bf79f50da13942a558ef5f
Chienping Tsung <jianpingzong@outlook.com>
2024-04-22 22:45:05 +0800

    Update component.py

--- lib/model/component.py
+++ lib/model/component.py
@@ -4,70 +4,6 @@ from torch import nn
 from torchvision import ops
 
 
-class PatchEmbed(nn.Module):
-    def __init__(self, patch_size=16, in_channels=3, embed_dim=768, norm_layer=None, flatten=True):
-        super().__init__()
-
-        self.flatten = flatten
-
-        self.proj = nn.Conv2d(in_channels, embed_dim, patch_size, patch_size)
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
-
-    def forward(self, x):
-        x = self.proj(x)
-        if self.flatten:
-            x = rearrange(x, 'b c h w -> b (h w) c')
-        x = self.norm(x)
-        return x
-
-
-def one_dim_sincos_pos_emb(embed_dim, pos):
-    """One-dimensional sin cos position embedding.
-
-    Args:
-        embed_dim: Embedding dimension.
-        pos: Position.
-    """
-    assert embed_dim % 2 == 0
-
-    omega = torch.arange(embed_dim // 2)
-    omega = 2 * omega / embed_dim
-    omega = 1 / 10000 ** omega
-
-    pos = torch.flatten(pos)
-    x = torch.einsum('s,c->sc', pos, omega)
-
-    emb_sin = torch.sin(x)
-    emb_cos = torch.cos(x)
-    emb = torch.cat((emb_sin, emb_cos), dim=1)
-
-    return emb
-
-
-def two_dim_sincos_pos_emb(embed_dim, h, w, class_token=False):
-    """Two-dimensional sin cos position embedding.
-
-    Args:
-        embed_dim: Embedding dimension.
-        h: H of patches.
-        w: W of patches.
-        class_token: Whether embedding with class token. Default is False.
-    """
-    assert embed_dim % 2 == 0
-
-    grid_h, grid_w = torch.arange(h), torch.arange(w)
-    grid_h, grid_w = torch.meshgrid(grid_h, grid_w)
-
-    emb_h = one_dim_sincos_pos_emb(embed_dim // 2, grid_h)
-    emb_w = one_dim_sincos_pos_emb(embed_dim // 2, grid_w)
-    emb = torch.cat((emb_h, emb_w), dim=1)
-
-    if class_token:
-        emb = torch.cat((torch.zeros((1, embed_dim)), emb), dim=0)
-
-    return emb
-
-
 def conv(in_ch, out_ch, kernel_size=3, stride=1, padding=1, dilation=1, freeze_bn=False):
     return nn.Sequential(nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding, dilation, bias=False),
                          ops.FrozenBatchNorm2d(out_ch) if freeze_bn else nn.BatchNorm2d(out_ch),
@@ -106,4 +42,4 @@ class CornerPredictor(nn.Module):
         tl_x, tl_y = self.softmax(tl)
         br_x, br_y = self.softmax(br)
 
-        return torch.stack((tl_x, tl_y, br_x, br_y), dim=1)
+        return torch.stack((tl_x, tl_y, br_x, br_y), dim=1), x
