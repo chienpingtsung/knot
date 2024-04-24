3a2dad3978b2099af6b365dcf088852771f391f2
Chienping Tsung <jianpingzong@outlook.com>
2024-04-24 18:02:37 +0800

    Update emb.py

--- jiu/models/emb.py
+++ jiu/models/emb.py
@@ -36,10 +36,10 @@ def two_dim_sincos_pos_emb(emb_dim, h, w, class_token=False):
 
 
 class PatchEmbed(nn.Module):
-    def __init__(self, patch_size=16, in_channels=3, emb_dim=768, norm_layer=None, flatten=True):
+    def __init__(self, patch_size=16, in_channels=3, emb_dim=768, norm_layer=None, output_fmt=None):
         super().__init__()
 
-        self.flatten = flatten
+        self.output_fmt = output_fmt
 
         self.proj = nn.Conv2d(in_channels, emb_dim, patch_size, patch_size)
         self.norm = norm_layer(emb_dim) if norm_layer else nn.Identity()
@@ -47,8 +47,8 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         x = self.proj(x)
 
-        if self.flatten:
-            x = einops.rearrange(x, 'b c h w -> b (h w) c')
+        if self.output_fmt:
+            x = einops.rearrange(x, f'b c h w -> {self.output_fmt}')
 
         x = self.norm(x)
 
